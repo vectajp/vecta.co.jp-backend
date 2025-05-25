a48e7d6a33a215ea507b5d61b90dfb656cb3cf17
Yuki Sakurai <sakurai.yuki@nck.co.jp>
2025-05-25 10:56:23 +0900

    feat: chanfanaとzodを追加し、APIエンドポイントを実装

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import { fromHono } from 'chanfana'
 import { Hono } from 'hono'
+import { TaskCreate } from './endpoints/taskCreate'
+import { TaskDelete } from './endpoints/taskDelete'
+import { TaskFetch } from './endpoints/taskFetch'
+import { TaskList } from './endpoints/taskList'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const openapi = fromHono(app, {
+  docs_url: '/',
 })
+
+openapi.get('/api/tasks', TaskList)
+openapi.post('/api/tasks', TaskCreate)
+openapi.get('/api/tasks/:taskSlug', TaskFetch)
+openapi.delete('/api/tasks/:taskSlug', TaskDelete)
 
 export default app
